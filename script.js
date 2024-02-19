914f9327757560caf468eaa74325197828d6cd4c
Abhishek Jha <avishekjha3511@gmail.com>
2024-02-19 15:50:15 +0530

    my-portfolio

---
+++ script.js
@@ -0,0 +1,18 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//   // Number of cards to repeat
+//   const numberOfCards = 5;
+
+//   // Container to hold the cards
+//   const container = document.getElementsByClassName('skills-sec');
+
+//   // Loop to creat and append the cards
+//   for (let i = 0; i < numberOfCards; i++) {
+//     // Create a card element
+//     const card = document.createElement('div')
+//     card.className = 'skill-card'
+//     // card.textContent = `Card ${i +1}`    
+
+//     container.appendChild(card)
+//   }
+  
+// })
